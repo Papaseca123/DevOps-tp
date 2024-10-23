6be6847a7d5a61152c97365a92d6fe3c4c5bf963
ricaldimd15@gmail.com <ricaldimd15@gmail.com>
2024-10-22 21:00:05 -0400

    testeo

--- test/example.test.js
+++ test/example.test.js
@@ -1,10 +1,14 @@
 const request = require('supertest');
-const { app } = require('../index'); 
+const { app } = require('../index'); // Asegúrate de exportar correctamente tu app en index.js
 
-describe('GET /api/comidas/2', () => {
-  it('debería devolver pizza', async () => {
-    const res = await request(app).get('/api/comidas/2');  
-    expect(res.statusCode).toEqual(200);                 
-    expect(res.body).toHaveLength(1);                    
+describe('GET /api/comidas/:idcomida', () => {
+  it('Debería devolver la comida con id 2', async () => {
+    const response = await request(app).get('/api/comidas/2');
+    expect(response.status).toBe(200);
+    expect(response.body).toMatchObject({
+      idcomida: 2,
+      nombre: "pizza",
+      costo: 4000
+    });
   });
 });
